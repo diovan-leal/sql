db0e8bbf395f823120af20b1094fa9c31c606e3d
diovan-leal <47063082+diovan-leal@users.noreply.github.com>
2022-04-11 23:03:40 -0300

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -43,7 +43,48 @@ SELECT
     FROM atl_order_payment aop
     WHERE aop.order_id IN (SELECT aut.order_id 
                              FROM atl_user_ticket aut)
-  GROUP BY aop.order_id  
+  GROUP BY aop.order_id 
+  
+  --------------------------------------------
+  -- SQL 4
+  
+  SELECT /*aot.price, aot.fee, aot.total, aot.quantity, aot.tax_type, aot.tax_value, sum(orders.total_value) as total_value,*/
+	   sum(CASE 
+	   		WHEN aot.fee = 0 
+	   			THEN (aot.total - (aot.tax_value - aot.quantity)) - IFNULL(o.discount, 0) 
+	   			ELSE aot.total - (aot.fee * aot.quantity) - IFNULL(o.discount, 0)
+	   		END) valueToReceive
+  FROM atl_order_ticket aot
+  JOIN (SELECT ao.id,
+  			   ao.discount
+  		  FROM atl_order ao
+ 		 WHERE ao.event_id = 250
+   		   AND ao.is_deleted = 0
+   		   AND ao.status_id IN (4, 5)) o ON o.id = aot.order_id 
+  LEFT JOIN (SELECT aop2.order_id, aop2.pay_id, aop2.amount total_value
+  			   FROM  atl_order_payment aop2
+ 			  WHERE aop2.order_id IN (SELECT ao.id 
+  										FROM atl_order ao
+ 									   WHERE ao.event_id = 250
+   										 AND  ao.is_deleted = 0
+   										 AND ao.status_id IN (4, 5))
+   				AND aop2.pay_id IS NOT NULL 
+   				AND aop2.pay_id  <> ''
+   				AND aop2.pay_id NOT IN ('PAYMENT_OVERDUE', 'PAYMENT_DELETED', 'PENDING', 'PAYMENT_REFUNDED')
+		   GROUP BY aop2.pay_id, aop2.amount, aop2.order_id) orders ON orders.order_id = aot.order_id
+WHERE aot.order_id IN (SELECT ao.id 
+  						 FROM atl_order ao
+ 						WHERE ao.event_id = 250
+   						  AND   ao.is_deleted = 0
+   						  AND ao.status_id IN (4, 5))
+  AND aot.price IS NOT NULL
+  /*GROUP BY aot.id, 
+           aot.fee,
+           aot.total,
+		   aot.quantity, 
+		   aot.tax_type, 
+		   aot.tax_value, 
+		   orders.order_id*/
 
  
  
